b85794a531e2ca387ef8680fde41be0ef5330436
RUSHABH82 <makanirushabh0786@gmail.com>
2023-07-11 19:43:50 +0530

    auth complated

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,24 +1,34 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as CrownLogo } from "../../../assets/crown.svg";
 
-import './navigation.styles.scss';
+import "./navigation.styles.scss";
+import { UserContext } from "../../context/user.context";
+import { signOutUser } from "../../../utils/firebase/firebase.utils";
 
 export const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <CrownLogo className="logo"/>
+          <CrownLogo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
             <div>SHOP</div>
           </Link>
-          <Link className="nav-link" to="/auth">
-            <div>SIGN IN</div>
-          </Link>          
+          {currentUser ? (
+            <span onClick={signOutUser} className="nav-link">
+              Logout
+            </span>
+          ) : (
+            <Link className="nav-link" to="/auth">
+              <div>SIGN IN</div>
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
