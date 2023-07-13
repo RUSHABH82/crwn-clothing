4a45e9f0c74fc547c42cf154f319f685931527fe
RUSHABH82 <makanirushabh0786@gmail.com>
2023-07-13 16:05:51 +0530

    add checkout items with total price.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./cart-dropdown.styles.scss";
 import { Button } from "../button/button.component";
+import { CartContext } from "../context/cart.context";
+import { CartItem } from "../cart-item/cart-item.component";
+import { useNavigate } from "react-router-dom";
 
 export const CartDropdown = () => {
+  const { cartItems, setIsCartOpen } = useContext(CartContext);
+
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    setIsCartOpen(false);
+    navigate("/checkout");
+  };
+
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>CHECKOUT</Button>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button } from "../button/button.component";
-import "./product-card.styles.scss"
+import "./product-card.styles.scss";
+import { CartContext } from "../context/cart.context";
 
 export const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => {
+    addItemToCart(product);
+  };
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`} />
@@ -11,7 +19,9 @@ export const ProductCard = ({ product }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <Button buttonType="inverted"> Add to cart</Button>
+      <Button buttonType="inverted" onClick={addProductToCart}>
+        Add to cart
+      </Button>
     </div>
   );
 };
